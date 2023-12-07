12654b2528e5e962acc52b6aa1ae03214e2486b4
Shiva <archandadhaniya@yahoo.com>
2023-12-07 13:49:12 +0530

    datatype

---
+++ 01_basic/02_data_types.js
@@ -0,0 +1,11 @@
+"use strict" ;  //treat all JS code as a newer version
+
+// alert (3+3) // we are using the nodejs, Not browser. 
+
+let name = "Archan"
+let age = 18
+let isLogged = false
+
+
+console.log(typeof undefined)
+console.log(typeof null)
