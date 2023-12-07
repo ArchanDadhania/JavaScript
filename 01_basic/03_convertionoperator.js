bae030b7e52d70217065026229eb9ce3bcd4d89c
Shiva <archandadhaniya@yahoo.com>
2023-12-07 14:54:15 +0530

    convertion done.

---
+++ 01_basic/03_convertionoperator.js
@@ -0,0 +1,25 @@
+// let variable = 33
+
+
+// console.log(typeof variable)
+// console.log(typeof(variable))
+
+
+// let valueInnumber = Number(variable)
+
+// console.log(typeof valueInnumber)
+// console.log(valueInnumber)
+
+let isLoggedin = ""
+
+let booleanIsLoggedin = Boolean(isLoggedin)
+console.log(booleanIsLoggedin)
+
+// 1 => true : 0 => false; 
+// ""=> false; "Archan" => true 
+
+let string = 33
+
+let thisisnumber = String(string)
+
+console.log(typeof thisisnumber)
