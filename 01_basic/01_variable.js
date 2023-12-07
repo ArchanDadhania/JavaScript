33120e2da0182188ff12819da5c28625fc33e7ef
Shiva <archandadhaniya@yahoo.com>
2023-12-07 13:33:39 +0530

    change in variable.

--- 01_basic/01_variable.js
+++ 01_basic/01_variable.js
@@ -2,6 +2,7 @@ const accoutId = 12345678
 let accountEmailId = "archandadhaniya@yahoo.com"
 var accountPassword = "12345"
 accountCity = "Vadodara"
+let accountState
 
 
 // accoutId = 2 
@@ -10,7 +11,13 @@ accountEmailId = "sunilpanchani@gmail.com"
 accountPassword = "2121"
 accountCity = "Rajkot`"
 
+
+/*
+prefer not to use var
+because issue in block scope and functional scope 
+*/
+
 console.log(accoutId);
 
-console.table([accountEmailId,accountPassword,accoutId,accountCity])
+console.table([accountEmailId,accountPassword,accoutId,accountCity,accountState])
 
