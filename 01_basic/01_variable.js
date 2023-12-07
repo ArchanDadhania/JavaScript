18f76bd1b8907999779838519ebfe23920211bb3
ArchanDadhania <59695959+ArchanDadhania@users.noreply.github.com>
2023-12-07 07:45:07 +0000

    variable function

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,15 @@
+const accoutId = 12345678
+let accountEmailId = "archandadhaniya@yahoo.com"
+var accountPassword = "12345"
+accountCity = "Vadodara"
+
+
+// accoutId = 2 
+
+accountEmailId = "sunilpanchani@gmail.com"
+accountPassword = "2121"
+accountCity = "Rajkot`"
+
+console.log(accoutId);
+
+console.table
